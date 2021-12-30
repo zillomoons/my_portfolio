d7575bd8d5dd2547718e36325396804119cdc8ee
zillomoons <dlzht73@ya.ru>
2021-12-30 19:08:29 +0300

    refactoring

--- src/contact_form/ContactForm.js
+++ src/contact_form/ContactForm.js
@@ -10,44 +10,55 @@ export const ContactForm = () => {
             <div className={`${styleContainer.container} ${s.formContainer}`}>
                 <SectionHeading smallHeading='get in touch' mainHeading='contact me.'/>
                 <div className={s.contactsContainer}>
-                    <div className={s.contactAddress}>
-                        <div className={s.singleAddress}>
-                            <h6><i><BsMap/></i> Address:</h6>
-                            <a href="">Moscow, Russia</a>
-                        </div>
-                        <div className={s.singleAddress}>
-                            <h6><i><AiOutlineMail/></i> Email:</h6>
-                            <a href="">dolzhit.ts@gmail.com</a>
-                        </div>
-                        <div className={s.singleAddress}>
-                            <h6><i><BsTelephone/></i> Phone:</h6>
-                            <a href="">+7 898 545 45 45</a>
-                        </div>
-                    </div>
-                    <form className={s.form}>
-                        <div className={s.formRow}>
-                            <div className={s.formGroup}>
-                                <i><IoPersonOutline/></i>
-                                <input type="text" placeholder='Name'/>
-                            </div>
-                            <div className={s.formGroup}>
-                                <i><AiOutlineMail/></i>
-                                <input type="email" placeholder='Email'/>
-                            </div>
-                        </div>
-                        <div className={s.formGroup}>
-                            <i><AiOutlineFileText/></i>
-                            <input type="text" placeholder='Subject'/>
-                        </div>
-                        <div className={s.formGroup}>
-                            <i><AiOutlineMessage/></i>
-                            <textarea placeholder='Message'/>
-                        </div>
-                        <button className={s.submitBtn} type='submit'>Send message</button>
-                    </form>
+                    <ContactAddress />
+                    <InnerForm />
                 </div>
-
             </div>
         </div>
+    )
+}
+
+const contacts = [
+    {title: 'Address', icon: BsMap, description: 'Moscow, Russia', href: ''},
+    {title: 'Email', icon: AiOutlineMail, description: 'dolzhit.ts@gmail.com', href: ''},
+    {title: 'Phone', icon: BsTelephone, description: '+7 898 545 45 45', href: ''},
+]
+
+const ContactAddress = () =>{
+    return (
+        <div className={s.contactAddress}>
+            {contacts.map(c => <div key={c.title} className={s.singleAddress}>
+                <h6>
+                    <i>{React.createElement(c.icon, null, null)}</i>
+                    {c.title}
+                </h6>
+                <a href={c.href}>{c.description}</a>
+            </div>)}
+        </div>
+    )
+}
+const InnerForm = () => {
+    return (
+        <form className={s.form}>
+            <div className={s.formRow}>
+                <div className={s.formGroup}>
+                    <i><IoPersonOutline/></i>
+                    <input type="text" placeholder='Name'/>
+                </div>
+                <div className={s.formGroup}>
+                    <i><AiOutlineMail/></i>
+                    <input type="email" placeholder='Email'/>
+                </div>
+            </div>
+            <div className={s.formGroup}>
+                <i><AiOutlineFileText/></i>
+                <input type="text" placeholder='Subject'/>
+            </div>
+            <div className={s.formGroup}>
+                <i><AiOutlineMessage/></i>
+                <textarea placeholder='Message'/>
+            </div>
+            <button className={s.submitBtn} type='submit'>Send message</button>
+        </form>
     )
 }
