c507b90acb9ded6cad67d9e96880c6a360ce8854
zillomoons <dlzht73@ya.ru>
2021-12-30 20:32:17 +0300

    refactoring

--- src/skills/Skills.js
+++ src/skills/Skills.js
@@ -1,16 +1,18 @@
 import React from "react";
 import s from './Skills.module.css';
 import stylesContainer from '../common/styles/Container.module.css';
-import {Skill} from "./skill/Skill";
 import {SectionHeading} from "../components/section-heading/SectionHeading";
-import {FaReact, SiRedux, SiTypescript, SiJavascript, SiHtml5 } from "react-icons/all";
+import {FaReact, SiRedux, SiTypescript, SiJavascript, SiHtml5, FaServer,  VscDebugRestartFrame, SiMaterialui} from "react-icons/all";
 
 const skills = [
-    {id: 3, icon: FaReact, title: "React", description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, magna aliqua. '},
-    {id: 4, icon: SiRedux, title: "Redux", description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, magna aliqua. '},
-    {id: 5, icon: SiTypescript, title: "TypeScript", description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, magna aliqua. '},
-    {id: 1, icon: SiJavascript, title: "JavaScript", description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. '},
-    {id: 2, icon: SiHtml5,  title: "HTML & CSS", description: 'Lorem ipsum dolor sit amet. '},
+    {id: 1, icon: FaReact, title: "React", description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, magna aliqua. '},
+    {id: 2, icon: SiRedux, title: "Redux", description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, magna aliqua. '},
+    {id: 3, icon: SiTypescript, title: "TypeScript", description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, magna aliqua. '},
+    {id: 4, icon: SiJavascript, title: "JavaScript", description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. '},
+    {id: 5, icon: FaServer, title: "Rest API", description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. '},
+    {id: 6, icon: VscDebugRestartFrame, title: "Unit tests", description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. '},
+    {id: 7, icon: SiHtml5,  title: "HTML & CSS", description: 'Lorem ipsum dolor sit amet. '},
+    {id: 8, icon: SiMaterialui,  title: "Material UI", description: 'Lorem ipsum dolor sit amet. '},
 ]
 
 export const Skills = () =>{
@@ -26,6 +28,18 @@ export const Skills = () =>{
                     {mappedSkills}
                 </div>
             </div>
+        </div>
+    )
+}
+
+const Skill = (props)=> {
+    return (
+        <div className={s.skill}>
+            <div className={s.imgContainer}>
+                {React.createElement(props.icon, null, null)}
+            </div>
+            <h6>{props.title}</h6>
+            <p>{props.description}</p>
         </div>
     )
 }
