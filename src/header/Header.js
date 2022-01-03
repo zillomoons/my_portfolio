8693346c71a4f0930ad5a0725c2ab72ee0c8f69f
zillomoons <dlzht73@ya.ru>
2022-01-03 14:03:16 +0300

    refactored navmenu

--- src/header/Header.js
+++ src/header/Header.js
@@ -1,17 +1,43 @@
-import React, {useState} from 'react';
-import s from './Header.module.css';
-import {Navbar} from "../navbar/Navbar";
+import React, {useEffect, useState} from 'react';
 import {GiHamburgerMenu} from "react-icons/all";
+import {AccentSpan, Bars, Nav, NavbarContainer, NavbarLink, NavLogo, NavMenu} from "./navbarElements";
 
 export const Header = () => {
     const [sidebar, setSidebar] = useState(false);
-    const showSidebar = () => setSidebar(!sidebar)
+    const [scrollNav, setScrollNav] = useState(false);
+    const showSidebar = () => setSidebar(!sidebar);
+    const toggleHeader = () => {
+        window.scrollY >= 80
+            ? setScrollNav(true)
+            : setScrollNav(false)
+    }
+
+    useEffect(()=>{
+        window.addEventListener('scroll', toggleHeader)
+    }, [])
+
     // should use styled components because css module mess with added classes
     return (
-        <div className={s.header}>
-            <div className={s.logo}><a href="#top">Dolzhi<span>t</span></a> </div>
-            <span className={s.burgerMenu} onClick={showSidebar}><GiHamburgerMenu /></span>
-            <Navbar sidebar={sidebar}/>
-        </div>
+        <Nav scrollNav={scrollNav}>
+            <NavbarContainer>
+                <NavLogo href="#top">Dolzhi<AccentSpan>t</AccentSpan></NavLogo>
+                <Bars onClick={showSidebar}><GiHamburgerMenu/></Bars>
+                <NavMenu className={sidebar ? 'active' : ''}>
+                    {NavbarLinks.map((item, i)=>{
+                        return <NavbarLink key={i} href={item.href} onClick={showSidebar}>
+                            {item.title}
+                        </NavbarLink>
+                    })}
+                </NavMenu>
+            </NavbarContainer>
+
+        </Nav>
     )
 }
+
+const NavbarLinks = [
+    {title: 'Main', href: '#main'},
+    {title: 'Skills', href: '#skills'},
+    {title: 'Projects', href: '#projects'},
+    {title: 'Contacts', href: '#contacts'},
+]
---
+++ src/header/navbarElements.js
@@ -0,0 +1,91 @@
+import styled from "styled-components";
+
+export const Nav = styled.nav`
+  background: ${({scrollNav}) => (scrollNav ? '#fff' : 'transparent')};
+  height: 80px;
+  margin-top: -80px;
+  display: flex;
+  justify-content: center;
+  font-size: 1rem;
+  position: sticky;
+  top: 0;
+  z-index: 10;
+
+  @media screen and (max-width: 960px) {
+    transition: 0.8s all ease;
+  }
+`
+export const NavbarContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 80px;
+  z-index: 1;
+  width: 100%;
+  padding: 0 24px;
+  max-width: 1200px;
+`
+
+export const NavLogo = styled.a`
+  font-weight: 700;
+  font-size: 21px;
+  letter-spacing: 1.5px;
+  color: rgb(33, 37, 41);
+  font-family: 'Montserrat Alternates', sans-serif;
+  
+  &:hover{
+    color: rgb(33, 37, 41);
+  }
+`
+export const AccentSpan = styled.span`
+  color: #47bb8e;
+`
+
+export const Bars = styled.span`
+  align-self: center;
+  color: #212529;
+  font-size: 30px;
+  height: 30px;
+
+  @media (min-width: 992px){
+      display: none;
+    }
+`
+
+export const NavMenu = styled.div`
+  display: flex;
+  justify-content: space-between;
+
+  @media screen and (max-width: 992px) {
+    flex-direction: column;
+    background: #212529;
+    position: fixed;
+    z-index: 500;
+    width: 100%;
+    height: 50%;
+    top: 60px;
+    right: -100%;
+    transition: 850ms;
+    padding-top: 50px;
+
+    &.active{
+      right: 0;
+      transition: 350ms;
+      margin: 0 auto;
+
+    }
+  }
+`
+
+export const NavbarLink = styled.a`
+  color: #212529;
+  padding: 13px 7px;
+  margin: 10px 15px;
+  font-size: 13px;
+  font-weight: 500;
+  display: block;
+
+  @media screen and (max-width: 992px) {
+    color: white;
+  }
+`
