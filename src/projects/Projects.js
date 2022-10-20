e5fd377b471260abec2c1abe62dd5aaca1872a41
Zillomoons <dlzht73@ya.ru>
2022-10-20 16:39:42 +0800

    adding links

--- src/projects/Projects.js
+++ src/projects/Projects.js
@@ -5,13 +5,38 @@ import {Project} from "./project/Project";
 import {SectionHeading} from "../components/section-heading/SectionHeading";
 import socNet from '../assets/soc_network.jpg';
 import todoImg from '../assets/todo.png';
-import counter from '../assets/counter.png';
+import chatApp from '../assets/chat_app.png';
 
 const projects = [
-    {id: 1, bgImage: { backgroundImage: 'url(' + socNet + ')' }, title: 'Social Network', href: 'https://github.com/zillomoons/social_network_ts', description: 'SPA created with React JS library, Redux for state management, TypeScript for scalability, Material UI for beauty.'},
-    {id: 2, bgImage: {backgroundImage: 'url(' + todoImg + ')'}, title: 'TODO List', href: 'https://github.com/zillomoons/todolist-ts_styled_components', description: 'Application build with ReactJS/Redux/TypeScript and styled components library.'},
-    {id: 3, bgImage: {backgroundImage: 'url(' + counter + ')'}, title: 'Counter', href: 'https://github.com/zillomoons/counter', description: 'Application build with ReactJS/Redux/TypeScript.'},
-]
+  {
+    id: 1,
+    bgImage: { backgroundImage: 'url(' + socNet + ')' },
+    title: 'Social Network',
+    href: 'https://github.com/zillomoons/social_network_ts',
+    code: 'https://github.com/zillomoons/social_network_ts',
+    description:
+      'SPA created with React JS library, Redux for state management, TypeScript for scalability, Material UI for beauty.',
+  },
+  {
+    id: 2,
+    bgImage: { backgroundImage: 'url(' + todoImg + ')' },
+    title: 'TODO List',
+    href: 'https://disk.yandex.ru/i/B4gwCBWpO2Hhwg',
+    code: 'https://github.com/zillomoons/todolist-ts_styled_components',
+    description:
+      'Application build with ReactJS/Redux/TypeScript and styled components library.',
+  },
+  {
+    id: 3,
+    bgImage: { backgroundImage: 'url(' + chatApp + ')' },
+    title: 'Chat App',
+    href: 'https://disk.yandex.ru/i/IlD-RvwE-KqBZg',
+    code: 'https://github.com/zillomoons/chat_app_ts',
+    description:
+      'Application build with ReactJS/TypeScript and NodeJS for backend.',
+  },
+];
+
 
 
 export const Projects = () => {
@@ -22,6 +47,7 @@ export const Projects = () => {
         style={p.bgImage}
         title={p.title}
         icon={p.icon}
+        code={p.code}
         description={p.description} />)
     return (
         <div id='projects' className={s.projectsBlock}>
--- src/projects/project/Project.js
+++ src/projects/project/Project.js
@@ -3,15 +3,25 @@ import s from './Project.module.css';
 
 export const Project = (props) => {
     return (
-        <div className={s.project}>
-            <div className={s.imgContainer} style={props.style}>
-                <a href={props.href} rel="noreferrer" target="_blank" >Watch</a>
-            </div>
-            <div className={s.description}>
-                <h5 className={s.title}>{props.title}</h5>
-                <p>{props.description}</p>
-            </div>
-
+      <div className={s.project}>
+        <div className={s.imgContainer} style={props.style}>
+          <a href={props.href} rel='noreferrer' target='_blank'>
+            Watch
+          </a>
         </div>
-    )
+        <div className={s.description}>
+          <h5 className={s.title}>{props.title}</h5>
+          <p>{props.description}</p>
+          <p>
+            <a
+              href={props.code}
+              target='_blank'
+              rel='noreferrer'
+            >
+              Code
+            </a>
+          </p>
+        </div>
+      </div>
+    );
 }
